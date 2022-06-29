51f3d9763ef99f0384dfc2de9a4d89a0209ebc71
SergioPrinci <sergioprinci03@live.com>
2022-06-29 19:44:14 +0200

    buttons fix

--- script.js
+++ script.js
@@ -86,6 +86,13 @@ async function main(stringList)
 
 async function loadThirdYear()
 {
+    var buttons = document.getElementsByTagName("button");
+
+    for (var i = 0; i < buttons.length; i++)
+    {
+        buttons[i].remove();
+    }
+
     terminal.innerHTML = "";
 
     stringList = [

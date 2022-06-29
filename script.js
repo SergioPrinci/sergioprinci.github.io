48696b419cc31f1e2f21caecb47a1bfb1eb921d4
SergioPrinci <sergioprinci03@live.com>
2022-06-29 19:18:21 +0200

    Modificato il testo in italiano e aggiunta fase di boot

--- script.js
+++ script.js
@@ -6,16 +6,52 @@ var stringList;
 
 async function main(stringList)
 {
-    var stringList = [" Booting up...", 
-                    "Found myself in: Italy",
-                    "...",
-                    "Well, that was quite unexpected.",
-                    "I don't know italian so, you'll get english.",
-                    "Hi Sergio, how are you doing?",
-                    "Not the right moment isn't it?",
-                    "Sorry, sorry, I'll try to focus.",
-                    "My job is to talk about your PCTO experiences.",
-                    "Now, Sergio, what do you want me to talk about?"];
+    //startup screen
+    terminal.innerHTML = "";
+
+    stringList = [
+        " *************** PIP-OS(R) V7.1.0.8 ***************",
+        "COPYRIGHT 2075 ROBCO(R)", 
+        "LOADER V1.1",
+        "EXEC VERSION 41.10",
+        "CPU TYPE: NVITEL 128x8",
+        "CPU CLOCK: 1.5GHz",
+        "4G RAM SYSTEM",
+        "32111938904 BYTES FREE",
+        "NO HOLOTAPE FOUND",
+        "LOAD ROM(1): PCTO.ROM",
+        "ROM SIGNAGE: sergioprinci",
+        "ROM VERSION: 2.3b",
+        "ROM DATE: 2022-07-08",
+        "ROM TIME: 00:00:00",
+        "ROM SIZE: 32596 BYTES",
+        "ROM CRC: 0x9D0A",
+        "ROM INTEGRITY: OK",
+        "LOADING CONTENT..."
+    ];
+
+    for (var i = 0; i < stringList.length; i++)
+    {
+        var string = stringList[i];
+        for (var j = 0; j < string.length; j++)
+        {
+            terminal.innerHTML += string[j];
+            await sleep(10);
+        }
+        terminal.innerHTML += "<br>";
+        await sleep(200);
+    }
+
+    await sleep(2000);
+
+    //normal screen
+    terminal.innerHTML = "";
+
+    stringList = [
+                " Booting up...",
+                "Benvenuti in PCTO Versione 2.3b.",
+                "Tramite questo sito vi verrá descritta la mia esperienza di PCTO in questi tre anni.",
+                "Prego, sciegliere l'anno di cui si vuole parlare."];
 
     for (var i = 0; i < stringList.length; i++)
     {
@@ -28,6 +64,7 @@ async function main(stringList)
         terminal.innerHTML += "<br>";
         await sleep(700);
     }
+    
     var buttonDiv = document.getElementById("buttons");
 
     var button1 = document.createElement("button");
@@ -47,12 +84,14 @@ async function main(stringList)
 
 async function loadThirdYear()
 {
-    stringList = [" Loading page...",
+    terminal.innerHTML = "";
+
+    stringList = [
+                " Loading page...",
                 "While you wait, I'll talk about what happened in your third year.",
-                "On the third year of your studies, the possibilities of PCTO experiences opened.",
+                "On the third year of your studies, the possibilities of PCTO experiences just opened.",
                 "I still remember how much you were excited about going to learn how to work..."];
 
-    terminal.innerHTML = "";
     for (var i = 0; i < stringList.length; i++)
     {
         var string = stringList[i];
